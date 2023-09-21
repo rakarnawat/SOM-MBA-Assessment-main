df3a84ed97d9d42787c0187d02a1ffda076dfbb4
Mohmed Anis Agwan <anishagwan@gmail.com>
2023-09-21 18:51:52 -0400

    deleted login backedn

--- Complete Project/Backend/bu-mba-assessment-login-registration-master/README.md
+++
@@ -1,2 +0,0 @@
-# bu-mba-assessment-login-registration
-Binghamton University - School of Management - MBA Assessment : Login &amp; Registration service
--- Complete Project/Backend/bu-mba-assessment-login-registration-master/src/main/java/bu/som/assessment/login/registration/Dto/ExistingUserDto.java
+++
@@ -1,9 +0,0 @@
-package bu.som.assessment.login.registration.Dto;
-
-import lombok.Data;
-
-@Data
-public class ExistingUserDto {
-    private String emailId;
-    private String password;
-}
