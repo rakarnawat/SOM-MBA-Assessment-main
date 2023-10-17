0ce83450da3fc4a33fa8e304fe3d81d3548fe187
Mohmed Anis Agwan <anishagwan@gmail.com>
2023-10-17 17:21:40 -0400

    Faculty sort

--- src/enums/role_enums.js
+++
@@ -1,7 +0,0 @@
-export const USER_ROLE = {
-  STUDENT: "student",
-  FACULTY: "faculty",
-  ADMIN: "admin",
-};
-
-Object.freeze(USER_ROLE);
--- src/enums/token_enums.js
+++
@@ -1,6 +0,0 @@
-export const TOKEN_ENUMS = {
-  REGISTER: "REGISTER",
-  FORGOT: "FORGOT",
-};
-
-Object.freeze(TOKEN_ENUMS);
